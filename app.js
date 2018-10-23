366bdc2a9b774dbb8b9209c573c6d4a036fdd265
tanguygarrel <42964051+tanguygarrel@users.noreply.github.com>
2018-10-23 12:58:28 +0200

    Final resume
    Updated version

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = new Vue({
     firstName: "Tanguy",
     lastName: "Garrel-Jaffrelot",
     description: "Je suis étudiant en journalisme et affaires internationales à Sciences Po Paris",
-    photo: "./Identity picture.jpeg",
+    photo: "Identity picture.jpeg",
     twitter: "@tanguyGarJaff",
     mail: "tanguy.garreljaffrelot@sciencespo.fr",
     experiences: [{
@@ -13,31 +13,34 @@ var app = new Vue({
       name: "Pizzeria Luigi",
       title: "Serveur",
       description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      logo: 'téléchargement.png',
+      logo: "téléchargement.png",
     }, {
       dateBegin: "Été 2013",
       dateEnd: false, 
       name: "Abattoirs de Cantoin",
       title: "Désosseur",
       description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      logo: "abattoir.png",
     }, {
       dateBegin: "2012",
       dateEnd: "2013",
       name: "Piscine de Palavas-les-Flos",
       title: "Stagiaire",
       description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      logo: "logo-piscine1.gif",
     }],
     Education: [{
       dateBegin: "2016",
       dateEnd: "2019",
       name: "Master Journalism & International Affairs",
       university: "Sciences Po Paris",
-      logo: "téléchargement.png",
+      logo: "téléchargement2.jpg",
     }, {
       dateBegin: "2013",
       dateEnd: "2016",
       name: "Bachelor of Social Sciences & Fundamental Sciences",
-      university: "Sciences Po Paris et Sorbonne Université"
+      university: "Sciences Po Paris et Sorbonne Université",
+      logo: "150_150_logo_su_vertical_seul_pantone.jpg",
     }],
     languages: ["Anglais", "Espagnol", "Arabe"],
     skills: ["Adobe Premier", "Pack Office", "D3.js", "CSS", "HTML"]
