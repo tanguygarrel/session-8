7707769dc6a280d9e6ae3b60df5e98735778d3be
tanguygarrel <42964051+tanguygarrel@users.noreply.github.com>
2018-10-23 12:23:44 +0200

    Project

--- app.js
+++ app.js
@@ -1,45 +1,46 @@
 var app = new Vue({
   el: '#app',
   data: {
-    firstName: "Jules",
-    lastName: "Bonnard",
-    description: "Je suis datajournaliste à l'AFP et j'enseigne le code, le traitement, et la visualisation de données à Sciences Po.",
-    photo: "https://pbs.twimg.com/profile_images/899285838516936704/nfeLD7he_400x400.jpg",
-    twitter: "julesbonnard",
-    github: "julesbonnard",
-    mail: "julesbonnard@gmail.com",
+    firstName: "Tanguy",
+    lastName: "Garrel-Jaffrelot",
+    description: "Je suis étudiant en journalisme et affaires internationales à Sciences Po Paris",
+    photo: "./Identity picture.jpeg",
+    twitter: "@tanguyGarJaff",
+    mail: "tanguy.garreljaffrelot@sciencespo.fr",
     experiences: [{
-      dateBegin: "2014",
+      dateBegin: false,
       dateEnd: "Today",
-      name: "Agence France Presse",
-      title: "Datajournalist",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      name: "Pizzeria Luigi",
+      title: "Serveur",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      logo: 'téléchargement.png',
     }, {
       dateBegin: "Été 2013",
-      dateEnd: false,
-      name: "Le Monde.fr",
-      title: "Data-journalist",
-      description: false
+      dateEnd: false, 
+      name: "Abattoirs de Cantoin",
+      title: "Désosseur",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     }, {
       dateBegin: "2012",
       dateEnd: "2013",
-      name: "Le Dauphiné Libéré",
-      title: "Stagiaire et correspondant de presse",
-      description: false
+      name: "Piscine de Palavas-les-Flos",
+      title: "Stagiaire",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     }],
-    formations: [{
-      dateBegin: "2012",
-      dateEnd: "2014",
-      name: "Master Journalisme",
-      university: "Sciences Po Grenoble"
+    Education: [{
+      dateBegin: "2016",
+      dateEnd: "2019",
+      name: "Master Journalism & International Affairs",
+      university: "Sciences Po Paris",
+      logo: "téléchargement.png",
     }, {
-      dateBegin: "2009",
-      dateEnd: "2012",
-      name: "Bachelor Sciences Politiques",
-      university: "Sciences Po Grenoble"
+      dateBegin: "2013",
+      dateEnd: "2016",
+      name: "Bachelor of Social Sciences & Fundamental Sciences",
+      university: "Sciences Po Paris et Sorbonne Université"
     }],
-    languages: ["Anglais"],
-    skills: ["Node", "VueJS", "D3js", "SCSS", "HTML"]
+    languages: ["Anglais", "Espagnol", "Arabe"],
+    skills: ["Adobe Premier", "Pack Office", "D3.js", "CSS", "HTML"]
   },
   computed: {
     fullName() {
